e06875fd56521c2e250fa44f14293fb2ef1e65fd
Michael Williams <mwilliams2964@gmail.com>
2019-12-06 16:21:02 -0700

    added budget file

---
+++ public/js/budget.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $("#budget-btn").on("click", function () {
+        event.preventDefault();
+        let mortgage = $("#mortgage").val().trim();
+        let food = $("#food").val().trim();
+        let utilities = $("#utilities").val().trim();
+        let insurance = $("#insurance").val().trim();
+        let transportation = $("#transportation").val().trim();
+        let fun = $("#fun").val().trim();
+        let savings = $("#savings").val().trim();
+        let misc = $("#misc").val().trim();
+        let debt = $("#debt").val().trim();
+
+        let budgetInfo = {
+            mortgage: mortgage,
+            food: food,
+            utilities: utilities,
+            insurance: insurance,
+            transportation: transportation,
+            fun: fun,
+            savings: savings,
+            misc: misc,
+            debt: debt
+        }
+
+        $
+            .post("/api/budget", { budgetInfo })
+            .then(console.log("successful post"));
+    })
+    
+})
