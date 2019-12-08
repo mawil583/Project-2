982fb4d28aca19e8dd2cc1ab2be06f5883c57137
Michael Williams <mwilliams2964@gmail.com>
2019-12-08 10:29:40 -0700

    made update route

--- public/js/addexpense.js
+++ public/js/addexpense.js
@@ -1,25 +1,40 @@
-$(document).ready(function() {
-    $("#placeholder").on("click", function() {
+$(document).ready(function () {
+    $("#expense").on("click", function () {
         event.preventDefault();
-        $.get("/api/chart", function (data) {
-            console.log(data);
-            // var mortgage = data.mortgage_b;
-            // var utilities = data.utilities_b;
-            // var food = data.food_b;
-            // var insurance = data.insurance_b;
-            // var transportation = data.transportation_b;
-            // var savings = data.savings_b;
-            // var debt = data.debt_b
-            // var fun = data.fun_b
-            var mortgageExp = data.mortgage_e;
-            var utilitiesExp = data.utilities_e;
-            var foodExp = data.food_e;
-            var insuranceExp = data.insurance_e;
-            var transportationExp = data.transportation_e;
-            var savingsExp = data.savings_e;
-            var debtExp = data.debt_e;
-            var funExp = data.fun_e;
-        });
-    })
+        console.log("expense button was clicked")
+        // var mortgageExp = data.mortgage_e;
+        // var utilitiesExp = data.utilities_e;
+        // var foodExp = data.food_e;
+        // var insuranceExp = data.insurance_e;
+        // var transportationExp = data.transportation_e;
+        // var savingsExp = data.savings_e;
+        // var debtExp = data.debt_e;
+        // var funExp = data.fun_e;
+        // let expenseArr = [mortgageExp, utilitiesExp, foodExp, insuranceExp, transportationExp, savingsExp, debtExp, funExp];
+       let expense = $("#price").val().trim();
+       let category = $("#expenses").val().trim();
+       let expenseObj = {
+           expense: expense,
+           category: `${category}_e`};
+           
+        $
+            .ajax({
+                method: "PUT",
+                url: "/api/expense",
+                data: expenseObj
+            })
+            .then(function () {
+                console.log("successful put request");
+            });
+        // var mortgage = data.mortgage_b;  
+        // var utilities = data.utilities_b;
+        // var food = data.food_b;
+        // var insurance = data.insurance_b;
+        // var transportation = data.transportation_b;
+        // var savings = data.savings_b;
+        // var debt = data.debt_b
+        // var fun = data.fun_b
+    });
 })
+
 
